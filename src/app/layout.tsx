e6ffc30628c682aa0aa119379b700519128c9fea
pcdosilva01-spec <pcdosilva01@gmail.com>
2026-05-10 23:57:08 -0300

    fix: adicionar SessionProvider ao root layout para corrigir useSession em prerender

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display, Cormorant_Garamond } from "next/font/google";
 import { Toaster } from "sonner";
+import { AuthProvider } from "@/components/shared/auth-provider";
 import "./globals.css";
 
 const inter = Inter({
@@ -71,7 +72,9 @@ export default function RootLayout({
       className={`${inter.variable} ${playfair.variable} ${cormorant.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
         <Toaster
           position="top-right"
           toastOptions={{
